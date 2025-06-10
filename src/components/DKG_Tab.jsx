a8918ba47296204759fc3823cb0985d8512cd792
Kishore DKG <kishore@dkgrouplabs.com>
2025-06-10 11:58:06 +0530

    Update DKG_Tab.jsx

--- src/components/DKG_Tab.jsx
+++ src/components/DKG_Tab.jsx
@@ -5,10 +5,10 @@ const Tab = ({icon, title, onClick, key}) => {
     <div 
           key={key} 
           onClick={onClick}
-          className={`cursor-pointer bg-darkBlue text-offWhite px-4 py-4 rounded-t-3xl rounded-l-3xl`}
+          className={`cursor-pointer bg-darkBlue text-offWhite px-4 py-4 rounded-t-3xl rounded-l-3xl text-center`}
         >
-          <span className='duty-tab-icon'>{icon}</span> <br /> <br />
-          <div>{title}</div>
+          <span className='duty-tab-icon'>{icon}</span>
+          <div className="mt-2">{title}</div> {/* Adjust margin as needed */}
         </div>
   )
 }
